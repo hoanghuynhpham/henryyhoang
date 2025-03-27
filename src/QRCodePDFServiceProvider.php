2662a8a7b57da697e025eaa494da24e7c0dde689
Phạm Hoàng Huynh <huynh.pham@smbplus.vn>
2025-03-27 15:08:11 +0700

    omg

--- src/QRCodePDFServiceProvider.php
+++ src/QRCodePDFServiceProvider.php
@@ -8,20 +8,37 @@ class QRCodePDFServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Load route từ package
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // Load routes nếu có
+        if (file_exists(__DIR__.'/../routes/web.php')) {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        }
 
-        // Load views từ package
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'package');
+        // Load views nếu có
+        if (is_dir(__DIR__.'/../resources/views')) {
+            $this->loadViewsFrom(__DIR__.'/../resources/views', 'hoanghuynh');
+            $this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/hoanghuynh'),
+            ], 'views');
+        }
 
-        // Publish view để người dùng có thể override nếu muốn
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/package'),
-        ], 'views');
+        // Load migrations nếu có
+        if (is_dir(__DIR__.'/../database/migrations')) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+
+        // Load cấu hình nếu có
+        if (file_exists(__DIR__.'/../config/hoanghuynh.php')) {
+            $this->publishes([
+                __DIR__.'/../config/hoanghuynh.php' => config_path('hoanghuynh.php'),
+            ], 'config');
+        }
     }
 
     public function register()
     {
-        //
+        // Merge config nếu package có file config
+        if (file_exists(__DIR__.'/../config/hoanghuynh.php')) {
+            $this->mergeConfigFrom(__DIR__.'/../config/hoanghuynh.php', 'hoanghuynh');
+        }
     }
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitad78d84ce5b091403afa125c76826b26
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitad78d84ce5b091403afa125c76826b26', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitad78d84ce5b091403afa125c76826b26', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitad78d84ce5b091403afa125c76826b26
         call_user_func(\Composer\Autoload\ComposerStaticInitad78d84ce5b091403afa125c76826b26::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitad78d84ce5b091403afa125c76826b26::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
